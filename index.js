45a13247da280b1454c21470931cca53ae2aec5d
vinimods2 <78501165+vinimods2@users.noreply.github.com>
2022-10-18 09:12:18 -0300

    update index.js

--- index.js
+++ index.js
@@ -242,4 +242,14 @@ async function start() {
 		console.log(err)
 	}
 }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update ${__filename}`))
+	delete require.cache[file]
+	require(file)
+})
+
+
 start()
